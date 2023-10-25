865436b223ee4fdd7e781bcc58591b380f73a23e
Fuji <139992158+Fujiwara-Haruki@users.noreply.github.com>
2023-10-25 16:51:11 -0300

    Tentativa falha de clicar p aparecer texto

---
+++ Js/scriptAplicacoes.js
@@ -0,0 +1,18 @@
+
+//funções aparecer texto
+function apliUm(){
+    var texto_1 = document.querySelector(".texto-um");
+    if (texto_1.style.display == "none"){
+        texto_1.style.display = "block";
+    } else{
+        texto_1.style.display = "none";
+    }
+}
+
+/*function apliDois(){
+
+}
+
+function apliTres(){
+
+}*/
